24528fca40cb1df95df2063867a234782300b729
tmicka23 <mickathp42@gmail.com>
2020-07-12 15:41:07 +0200

    add navbar with brand link and button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Header = () => {
-  return <h1>header</h1>;
+  return (
+    <nav className='navbar navbar-light bg-light'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='/'>
+          Audio Wizard
+        </a>
+        <div className='d-flex'>
+          <button className='btn btn-primary'>Mon compte</button>
+        </div>
+      </div>
+    </nav>
+  );
 };
-
 export default Header;
